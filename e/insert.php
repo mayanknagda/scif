58df3c40019ebaa40749251b60b51d4d09a6b5c4
Mayank Nagda <nagdamayank05@gmail.com>
2018-01-18 16:10:11 +0530

    Update insert.php

--- e/insert.php
+++ e/insert.php
@@ -36,8 +36,8 @@ try {
     $mail->isSMTP();                                      // Set mailer to use SMTP
     $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'theietsrmstudentchapter@gmail.com';                 // SMTP username
-    $mail->Password = 'ietsrm2017';                           // SMTP password
+    $mail->Username = 'yourmail.com';                 // SMTP username
+    $mail->Password = 'yourpassword';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
 
